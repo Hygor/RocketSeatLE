054710388183edd15258c5ffc4f85b74aa85d468
Hygor Budny <hygor@me.com>
2019-08-01 14:04:53 +0200

    Continuando API do GoBarber [ 2 /22 ]

---
+++ GoStack/Modulo03/src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+import User from '../app/models/User';
+import File from '../app/models/File';
+import DatabaseConfig from '../config/database';
+
+const models = [User, File];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(DatabaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
