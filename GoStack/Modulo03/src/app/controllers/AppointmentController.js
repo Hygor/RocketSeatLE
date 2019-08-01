bcb841d61879836c33bfff34c2b271384e010c89
Hygor Budny <hygor@me.com>
2019-08-01 21:10:30 +0200

    Continuando API do GoBarber [ 7 /22 ]

--- GoStack/Modulo03/src/app/controllers/AppointmentController.js
+++ GoStack/Modulo03/src/app/controllers/AppointmentController.js
@@ -1,10 +1,33 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO } from 'date-fns';
-import { isBefore } from 'date-fns/esm';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req,res) {
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+      {
+        model: User,
+        as: 'provider',
+        attributes: ['id', 'name'],
+        include: [{
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url']
+        }]
+      }
+      ]
+    })
+    return res.json(appointments);
+  }
   async store(req, res) {
     const schema = Yup.object().shape({
       date: Yup.date().required(),
@@ -30,9 +53,25 @@ class AppointmentController {
 
     const hourStart = startOfHour(parseISO(date));
 
+    // check date before
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({
         error: 'Past dates are not permited',
+      });
+    }
+
+    // check date availability
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      }
+    });
+
+    if ( checkAvailability ) {
+      return res.status(400).json({
+        error: 'Date is not available',
       });
     }
 
