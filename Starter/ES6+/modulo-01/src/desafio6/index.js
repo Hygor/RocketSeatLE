d98337accb6d88eba5448e13f771c9992f964f75
Hygor Budny <hygor@me.com>
2019-11-06 01:47:38 +0000

    progress/Starter: ES6+ Modulo-01 + desafios

---
+++ Starter/ES6+/modulo-01/src/desafio6/index.js
@@ -0,0 +1,9 @@
+// DESAFIO 5
+console.log('--- \nDesafio 6');
+
+const usuario = 'Diego';
+const idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+
+// template literals
+console.log(`O usuário ${usuario} possui ${idade} anos`);
--- Starter/ES6+/modulo-01/src/main.js
+++ Starter/ES6+/modulo-01/src/main.js
@@ -9,3 +9,12 @@ import desafio3 from './desafio3';
 
 // Desafio 4
 import desafio4 from './desafio4';
+
+// Desafio 5
+import desafio5 from './desafio5';
+
+// Desafio 6
+import desafio6 from './desafio6';
+
+// Desafio 7
+import desafio7 from './desafio7';
